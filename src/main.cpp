31650e5fd5892f200c7c61dcb1c9a29503ef1b2c
Serpent Dragon <shelyganovdmitry@gmail.com>
2024-07-09 12:31:11 +0300

    main function created

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
+#include "server/http_server.h"
 
 int main(int argc, char** argv)
 {
-    return 0;
+    if (argc != 3)
+    {
+        std::cout << "Usage: " << argv[0] << " <host> <port>\n";
+        return EXIT_FAILURE;
+    }
+
+    try
+    {
+        asio::io_service io;
+
+        http_server server(io, tcp::endpoint(asio::ip::address::from_string(argv[1]), std::stoi(argv[2])));
+        server.run();
+
+        io.run();
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
+    return EXIT_SUCCESS;
 }
